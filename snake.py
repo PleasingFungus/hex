eb145d14d7b0c307ba90cbd09ef7dd30f745fb1c
Nicholas Feinberg <pleasingfung@gmail.com>
2015-07-29 08:55:23 -0700

    Basic stair functionality

--- snake.py
+++ snake.py
@@ -1,9 +1,11 @@
 ''' Main snake game running processes. '''
 
 from area import Area
-from cell import CL_FLOOR
+from cell import CL_FLOOR, CL_STAIR
 from player import Player
 from point import Point
+
+level_dim = 19
 
 def run_game(render, io):
     ''' The main game loop.
@@ -11,9 +13,12 @@ def run_game(render, io):
             render (function<Area>): Render the current game state to the screen.
             io (function<Player, Area>): Query the player for their next action.
     '''
-    area = Area(Area.gen_room(20))
-    area.cells[Point(10, 10)] = CL_FLOOR()
-    player = area.cells[Point(10, 10)].actor = Player()
+    area = Area(Area.gen_room(level_dim))
+
+    halfwidth = int(level_dim / 2)
+    midpoint = Point(halfwidth, halfwidth)
+    area.cells[midpoint] = CL_FLOOR()
+    player = area.cells[midpoint].actor = Player()
 
     while True:
         render(area)
@@ -21,3 +26,28 @@ def run_game(render, io):
         if should_quit:
             break
 
+        check_stairs(player, area)
+
+def check_stairs(player, area):
+    ''' Check if the player is on the stairs.
+        If so, generate a new level.
+        Args:
+            player (Player): The character controlled by the player.
+            area (Area): The current level.
+    '''
+
+    loc = area.find_actor(player)
+    assert loc != None
+    assert loc in area.cells
+
+    cell = area.cells[loc]
+    if not cell.is_stairs:
+        return
+
+    # on the stairs; new level.
+    area.cells = Area.gen_room(level_dim)
+    assert loc in area.cells
+    area.cells[loc] = CL_FLOOR()
+    area.cells[loc].actor = player
+
+    area.cells[Point(loc.x, level_dim-1 - loc.y)] = CL_STAIR() # opposite the player, alternating
