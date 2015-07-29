63cf273f6b9f6fd00bcab2b514a4d60288570631
Nicholas Feinberg <nick.feinberg@gmail.com>
2015-07-29 13:39:03 -0700

    Add a sidebar

--- cmain.py
+++ cmain.py
@@ -3,7 +3,7 @@
 import logging
 
 from cinput.cimove import go
-from crender.crarea import render_area
+from crender.crarea import render_area, render_sidebar
 import curses
 from snake import run_game
 
@@ -11,9 +11,19 @@ def main(scr):
     ''' Run the game. '''
     curses.curs_set(0)
 
-    renderer = lambda area: render_area(area, scr)
+    sidebar_width = 12
+    main_width = curses.COLS - sidebar_width
+    sidebar_window = curses.newwin(curses.LINES, sidebar_width, 0, 0)
+    main_window = curses.newwin(curses.LINES, main_width, 0, sidebar_width) 
+
+    def render(area):
+        ''' Render info about the current area to the entire console. '''
+        render_area(area, main_window)
+        render_sidebar(area, sidebar_window)
+        # FIXME: this displays black until prompted for input (???)
     io = lambda player,area: go(scr.getkey(), player, area)
-    run_game(renderer, io)
+    run_game(render, io)
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
--- crender/crarea.py
+++ crender/crarea.py
@@ -11,3 +11,14 @@ def render_area(area, scr):
     for loc, cell in area.cells.items():
         scr.addstr(loc.y, loc.x, cell.cur_glyph().encode('utf-8'), cell.cur_color().attribute_code())
     scr.refresh()
+
+def render_sidebar(area, scr):
+    ''' Render a given Area's metadata to the console.
+
+    Args:
+        area (Area): The Area in question.
+        scr: the curses screen to be rendered to.
+    '''
+    scr.clear()
+    scr.addstr(0, 0, "Depth: {}".format(area.depth))
+    scr.refresh()
