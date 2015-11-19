e2be341a36c6dc76cd6af238064804d653a0f25a
Nicholas Feinberg <pleasingfung@gmail.com>
2015-11-18 18:44:02 -0800

    More enemy notes

--- mon.py
+++ mon.py
@@ -1,4 +1,10 @@
 ''' List of enemies '''
 # ghost - ignores terrain
-# sniper - fires from range
+# sniper - fires from range (is this redundant with lasers?)
+# cat - moves diagonally (single steps...?) (just worse mongoose?)
 # thorn - stationary, damages adjacent (incl. enemies?) (terrain?)
+# timed lasers - fire every n ticks along a line - (indicate firing path) - blocked by tail
+# triggered laser - fire 1 turn after path is entered
+# geysers - intermittently blow up in a point (or an area?)
+# mud - takes two turns to move through
+# fruit
